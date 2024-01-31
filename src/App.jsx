0dfdbee16db58688e105630224344dfe4f3c49a2
Onur Esin <onuresn034@gmail.com>
2024-01-31 22:13:20 +0300

    login ekranı yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
-import Login from './Components/Login'
+import { Link } from 'react-router-dom';
 
 function App() {
 
 
   return (
     <>
-      <div>
-        <link to={'login'} />
+      <div className="container">
+      <div className="loginSection">
+            <div className="loginTop">
+                <img src="/instagram-mp.svg" alt="Instagram main page" />
+                <h3>Arkadaşlarınızın fotoğraflarını ve videolarını görmek için <br/>giriş yapın veya kaydolun.</h3>
+            </div>
+            <div className="loginBot">
+                <button className='loginBtn'><Link to='register'>Giriş Yap</Link></button>
+            </div>
+        </div>
+            <footer>
+                <p>Hesabın yok mu?</p> <button className='registerBtn'><Link to='register'>Kaydol.</Link> </button>
+            </footer>
       </div>
     </>
   )
