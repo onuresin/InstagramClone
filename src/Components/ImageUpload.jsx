030db468e62a1b916fe8d4af855c14944d1224cf
Onur Esin <onuresn034@gmail.com>
2024-02-12 18:38:29 +0300

    imageupload kısmında güncelleme yapıldı

--- src/Components/ImageUpload.jsx
+++ src/Components/ImageUpload.jsx
@@ -4,7 +4,6 @@ import { supabase } from "./Feed";
 export default function ImageUpload() {
     const imagePreview = useRef(null);
     const [photo, setPhoto] = useState(null);
-    const [selfi, setSelfie] = useState(null)
 
     function handleChange(e) {
         const reader = new FileReader();
@@ -24,14 +23,6 @@ export default function ImageUpload() {
         });
         reader.readAsDataURL(e.target.files[0]);
     }
-    const handleSelfieChange = (e) => {
-        setSelfie(e.target.files[0]);
-        const reader = new FileReader();
-        reader.addEventListener('load', function(ev) {
-            imagePreview.current.src = ev.target.result;
-        });
-        reader.readAsDataURL(e.target.files[0]);
-    }
     
     async function handleSubmit(e) {
         e.preventDefault();
@@ -41,16 +32,15 @@ export default function ImageUpload() {
         const { data, error } = await supabase.storage
             .from('photos')
             .upload(formObj.image.name, formObj.image)
-            window.location.reload()
+            // window.location.reload()
     }
     return(
         <>
             <form className="uploadForm" onSubmit={handleSubmit}>
                 <input type="file" onChange={handleChange} accept="image/*" name="image" /> <br />
                 <label htmlFor="photo">Arka Kamera</label> <br />
-                <input type="file" capture="environment" onChange={handlePhotoChange} name="image" accept="image/*" /> <br />
-                <label htmlFor="selfie">Selfie</label> <br />
-                <input type="file" capture='user' onChange={handleSelfieChange} name="image" accept="image/*"/> <br /> <br />
+                <input type="file" capture="environment, user" onChange={handlePhotoChange} name="image" accept="image/*" /> <br />
+ <br />
                 <button className='loginBtn'>Yükle</button>
             </form>
 
